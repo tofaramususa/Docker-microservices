6947a757e058c146d2271079a351a2ab04be0615
Tofara Mususa <jtofaramususa@gmail.com>
2024-08-13 07:33:28 -0400

    working

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
 NAME = Inception
 
-all: start
+all: up
 
 
 # need explaination for difference between start and build below and the use of --env-file
 # what is the difference between up and build in docker
 up:
+	@echo "\033[33mLaunching configuration $(NAME)...\033[0m"
 	@bash srcs/requirements/wordpress/tools/make_dir.sh 
 	@docker compose -f srcs/docker-compose.yml --env-file srcs/.env up -d --build
 
-start:
-	@echo "\033[33mLaunching configuration $(NAME)...\033[0m"
-	@bash srcs/requirements/wordpress/tools/make_dir.sh 
-	@docker compose -f srcs/docker-compose.yml --env-file srcs/.env up -d 
+# start:
+# 	@bash srcs/requirements/wordpress/tools/make_dir.sh 
+# 	@docker compose -f srcs/docker-compose.yml --env-file srcs/.env up -d 
 
 
 down:
@@ -24,13 +24,16 @@ re: down up
 clean:
 	@echo "\033[33mCleaning configuration $(NAME)...\033[0m"
 	@docker system prune -a
-	@sudo rm -rf /home/tmususa/data/wordpress/*
-	@sudo rm -rf /home/tmususa/data/mariadb/*
+	@sudo rm -rf /home/tmususa/data/wordpress_vol/*
+	@sudo rm -rf /home/tmususa/data/mariadb_vol/*
 
 fclean: down
 	@echo "\033[33mTotal clean of all Docker configurations\033[0m"
 	@yes | docker system prune --all
-	@docker volume rm $$(docker volume ls)
+	@docker network prune --force
+	@docker volume prune --force
+	@sudo rm -rf /home/tmususa/data/wordpress_vol/*
+	@sudo rm -rf /home/tmususa/data/mariadb_vol/*
 # @rm -rf /home/tmususa/Desktop/inception/srcs/wordpress_vol /home/tmususa/Desktop/inception/srcs/mariadb_vol
 # @docker stop $$(docker ps -qa)
 # @docker system prune --all --force --volumes
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-# #DEBUG
-# echo "db name: ${DB_NAME}"
-# echo "root: ${DB_ROOT}"
-# echo "db pass: ${DB_PASS}"
+service mariadb start
 
-# # Start MariaDB in safe mode
-mysqld_safe --datadir='/var/lib/mysql' &
-# Check if the database exists
-# DB_EXISTS=$(mysql -u root -p ${DB_ROOT} -e "SHOW DATABASES LIKE '${DB_NAME}';" | grep ${DB_NAME} > /dev/null; echo "$?")
-# if [ $DB_EXISTS -eq 0 ]; then
-#     echo "The database ${DB_NAME} already exists."
-# else
-#     # Create the database if it doesn't already exist
-    mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-#     # Create a user and assign a password
-#     mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
-#     # Grant all privileges on the database to the user
-#     mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' WITH GRANT OPTION;"
-#     # Change the root user's password
-#     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';"
-#     # Refresh privileges to apply the changes
-#     mysql -u root -p"${DB_ROOT}" -e "FLUSH PRIVILEGES;"
-#     mysqladmin -u root -p$DB_ROOT shutdown
-#     echo "Configuration completed."
-# # fi
-# Restart MariaDB
-exec mysqld_safe --datadir='/var/lib/mysql'
+sleep 1
+
+if [ -d "/var/lib/mysql/$DB_NAME" ]
+then 
+
+	echo "Database already exists"
+else
+	mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+        mariadb  -e "CREATE USER IF NOT EXISTS ${DB_USER}@'localhost' IDENTIFIED BY '${DB_PASS}';"
+        mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';"
+        # mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';"
+        mariadb -e "FLUSH PRIVILEGES;"
+fi
+
+service mariadb stop
+
+mysqld_safe
